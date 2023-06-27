fea1ff5f3f02bc0c883fb85f128bd2c7fa0ef8fa
msalettes <matthieu.salettes@mirakl.com>
2023-06-27 12:29:17 +0200

    chore: add console.info to package3

--- packages/package3/src/lib/package3.ts
+++ packages/package3/src/lib/package3.ts
@@ -3,5 +3,6 @@ import { package2 } from "@changeset/package2";
 
 export function package3(): string {
   console.log('package3');
+  console.info('package3');
   return package2() + '-package3' + '-' + package1();
 }
