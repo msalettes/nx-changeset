8069916e891e185c9201ac6f1a44ce28e3769043
msalettes <matthieu.salettes@mirakl.com>
2023-06-27 12:30:05 +0200

    chore: add console.warn to package3

--- packages/package3/src/lib/package3.ts
+++ packages/package3/src/lib/package3.ts
@@ -2,5 +2,6 @@ import { package1 } from "@changeset/package1";
 import { package2 } from "@changeset/package2";
 
 export function package3(): string {
+  console.warn('package3');
   return package2() + '-package3' + '-' + package1();
 }
