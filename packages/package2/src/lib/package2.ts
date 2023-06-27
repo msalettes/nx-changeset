1a56cdb59ef84c47d9d1ceba966f1d3d2bdaefea
msalettes <matthieu.salettes@mirakl.com>
2023-06-27 12:23:00 +0200

    chore: add console.log to package2

--- packages/package2/src/lib/package2.ts
+++ packages/package2/src/lib/package2.ts
@@ -2,5 +2,6 @@ import { package1 } from "@changeset/package1";
 
 export function package2(): string {
   const package1Text = package1();
+  console.log(`package2`);
   return `${package1Text}-package2`;
 }
